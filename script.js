ae827091093b04477f024a429dbc34289d43dfd8
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-01-02 17:12:47 +0200

    1

---
+++ script.js
@@ -0,0 +1,18 @@
+let countClick = 0;
+let blockCount = document.querySelector("#count");
+let btn2 = document.querySelector("#btn2");
+let p2 = document.querySelector("#p2");
+let audioGun = document.querySelector("#audioGun");
+let btn3 = document.querySelector("#btn3");
+
+btn2.onclick = () => {
+  p2.style.background = "green";
+};
+btn3.onclick = () => {
+  audioGun.play();
+};
+
+function btnClick() {
+  countClick += 1;
+  blockCount.innerText = countClick;
+}
